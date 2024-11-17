11848f7bbe8fe287fb8b994202d4599534bd91b3
Mohamed Kosbar <kosbarmohamed.31@gmail.com>
2024-11-17 21:56:26 +0100

    Add files via upload

---
+++ Juvisy/index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const a = document.getElementById("a");
+
+    a.addEventListener("click", () => {
+        alert("La réservation en ligne sera disponible bientôt !");
+    });
+});
